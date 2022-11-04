6245b0ab7a345d13facaf23b7da1e346ce9a70a8
Leana Tabbara <lmt14@mail.aub.edu>
2022-11-04 10:59:04 +0200

    loading screen + pod file updates and some fixes

--- lib/views/loading_screen.dart
+++ lib/views/loading_screen.dart
@@ -6,14 +6,28 @@ class LoadingScreen extends StatelessWidget{
       WidgetsBinding.instance.window.devicePixelRatio;
   @override
   Widget build(BuildContext context){
-    return Container(
-      color: const Color(0xff900032),
-      child: Center(
-        child: Text(
-          "CAMPUS +",
-          textAlign: TextAlign.center,
+    return Material(
+      type: MaterialType.transparency,
+      child:Container(
+        color: const Color(0xff900032),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset('assets/campus_logo.png'),
+            Center(
+                child: Text(
+                  "CAMPUS +",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 50,
+                  ),
+                )
+            ),
+          ]
         )
       ),
     );
+
   }
 }
