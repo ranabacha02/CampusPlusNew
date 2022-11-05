f48b33415768c13cd50f601faafc6122f1774bdb
Leana Tabbara <lmt14@mail.aub.edu>
2022-11-05 13:43:50 +0200

    sign in / sign up screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:campus_plus/views/loading_screen.dart';
+import 'package:campus_plus/views/loading_screen.dart';
+import 'package:campus_plus/views/signIn_signUp_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
@@ -25,7 +27,7 @@ class MyApp extends StatelessWidget {
         Theme.of(context).textTheme, // If this is not set, then ThemeData.light().textTheme is used.
       ),
       ),
-      home:  LoadingScreen(),
+      home:  LoginView(),
     );
   }
 }
