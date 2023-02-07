60cc95f1aa5c24e11a2932ad10b756e3643ae353
Ali Yunes <117394409+azy99@users.noreply.github.com>
2023-02-07 19:45:39 +0200

    Update course controller

--- lib/controller/course_controller.dart
+++ lib/controller/course_controller.dart
@@ -1,23 +1,10 @@
-import 'dart:collection';
 import 'dart:io';
-import 'dart:ui';
-
-import 'package:campus_plus/model/user_model.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/services.dart';
 import 'package:get/get.dart';
-import 'package:get/get_core/src/get_main.dart';
-import 'package:get/get_state_manager/src/simple/get_controllers.dart';
 import 'package:firebase_storage/firebase_storage.dart';
-import 'package:page_transition/page_transition.dart';
-import 'package:path_provider/path_provider.dart';
-
 import '../model/clean_user_model.dart';
 import '../model/course_model.dart';
-import '../views/edit_account_screen.dart';
-import '../widgets/nav_bar.dart';
 import 'data_controller.dart';
 
 class CourseController extends GetxController {
@@ -27,38 +14,30 @@ class CourseController extends GetxController {
   DataController dataController = Get.put(DataController());
 
 
-
-
-  void createcourse(String event, String department, String price) {
+  void createCourse(String courseName, String department, String price) {
     CleanUser user = CleanUser.fromMyUser(dataController.getLocalData());
     MyCourse course= MyCourse(
         createdBy: auth.currentUser!.uid,
-        event: event,
+        courseName: courseName,
         department: department,
         price: price,
         user: user,
-
-
     );
     course.createCourse();
   }
 
 
-
-
-
-  Future<bool> removeCard(String cardId) async {
+  Future<bool> removeCourse(String cardId) async {
     return MyCourse.removeCourse(cardId);
   }
 
-  Future getStreamOfCards() async {
+  Future getStreamOfCourses() async {
     return MyCourse.getStreamOfCourses();
   }
 
-  Future getAllCards() async {
+  Future getAllCourses() async {
     return MyCourse.getAllCourses();
   }
-
 
   Future getMyCourses() async{
     final myCreatedCards = await MyCourse.getMyCreatedCourses(auth.currentUser!.uid);
