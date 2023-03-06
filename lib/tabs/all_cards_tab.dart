c0a278dca43827d6a752bf2fe5951ac23b681870
Ali Yunes <117394409+azy99@users.noreply.github.com>
2023-03-06 20:02:21 +0200

    Refactor home screen to use tabs for tags

---
+++ lib/tabs/all_cards_tab.dart
@@ -0,0 +1,152 @@
+import 'package:flutter/material.dart';
+import 'package:loading_animation_widget/loading_animation_widget.dart';
+import '../controller/card_controller.dart';
+import '../controller/data_controller.dart';
+import '../model/card_model.dart';
+import '../model/user_model.dart';
+import '../utils/app_colors.dart';
+import '../widgets/main_card.dart';
+import 'package:get/get.dart';
+
+class AllCardsTab extends StatefulWidget {
+  const AllCardsTab({Key? key}) : super(key: key);
+
+  @override
+  State<AllCardsTab> createState() => _AllCardsTabState();
+}
+
+class _AllCardsTabState extends State<AllCardsTab> {
+  final GlobalKey<RefreshIndicatorState> _refreshIndicatorKey = GlobalKey<RefreshIndicatorState>();
+  late DataController dataController;
+  late CardController cardController;
+  late final MyUser userInfo;
+  late Future<List<MyCard>> futureCards;
+  late List<MyCard> cards;
+  final ScrollController scrollController = ScrollController();
+  final limit = 10;
+  bool isLoadingMoreCards = false;
+
+  @override
+  void initState() {
+    super.initState();
+    dataController = Get.put(DataController());
+    cardController = Get.put(CardController());
+    userInfo = dataController.getLocalData();
+    futureCards = getInitialCards();
+    scrollController.addListener(_scrollListener);
+  }
+
+  Future<void> _scrollListener() async {
+    if(isLoadingMoreCards) return;
+    if(scrollController.position.pixels > scrollController.position.maxScrollExtent-200 && scrollController.position.pixels> 50){
+      setState(() {
+        isLoadingMoreCards = true;
+      });
+      await getNextCards(cards[cards.length-1]);
+      setState(() {
+        isLoadingMoreCards = false;
+      });
+    }
+  }
+
+  Future<List<MyCard>> getInitialCards() async {
+    cards = await cardController.getInitialCards(limit);
+    return cards;
+  }
+
+  Future<void> getNextCards(MyCard card) async {
+    final newCards = await cardController.getNextCards(card, limit);
+    cards = cards + newCards;
+    final combinedCards = Future.delayed(const Duration(microseconds: 1), () => cards);
+    setState(() {
+      futureCards = combinedCards;
+    });
+  }
+
+  Future<void> updateCard(String cardId) async {
+    final updatedCard = await cardController.getCardById(cardId);
+    if(updatedCard == null){
+      for(int i = 0; i< cards.length; i++){
+        if(cards[i].id == cardId){
+          cards.removeAt(i);
+        }
+      }
+    }
+    else{
+      for(int i = 0; i< cards.length; i++){
+        if(cards[i].id == cardId){
+          cards[i] = updatedCard;
+        }
+      }
+    }
+    final updatedCards = Future.delayed(const Duration(microseconds: 1), () => cards);
+    setState(() {
+      futureCards = updatedCards;
+    });
+  }
+
+  Future<void> updatePage() async {
+    final newCards = getInitialCards();
+    cards = await newCards;
+    await Future.delayed(const Duration(milliseconds: 300));
+    final updatedCards = Future.delayed(const Duration(microseconds: 1), () => cards);
+    setState(() {
+      futureCards = updatedCards;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        color: Colors.white,
+        child: FutureBuilder(
+          future: futureCards,
+          builder: (context, snapshot){
+            return RefreshIndicator(
+                key: _refreshIndicatorKey,
+                color: AppColors.aubRed,
+                backgroundColor: Colors.white,
+                strokeWidth: 2.0,
+                displacement: 20.0,
+                onRefresh: updatePage,
+                child: _listView(scrollController, isLoadingMoreCards, snapshot, userInfo, updateCard)
+            );
+          },
+        )
+    );
+  }
+}
+
+Widget _listView(ScrollController scrollController, bool isLoadingMoreCards, AsyncSnapshot snapshot, MyUser userInfo, Function updateCard) {
+  if(!snapshot.hasData){
+    return Center(child: CircularProgressIndicator(color: AppColors.aubRed));
+  }
+  if(snapshot.hasError){
+    return const Text('Something went wrong');
+  }
+  final cards = snapshot.data;
+  return ListView.builder(
+    controller: scrollController,
+    physics: const AlwaysScrollableScrollPhysics(),
+    itemCount: isLoadingMoreCards ? cards.length+1 : cards.length,
+    itemBuilder: (context, index){
+      if(index<cards.length){
+        if(cards[index].createdBy == userInfo.userId){
+          return MainCard(
+            card: cards[index],
+            personal: true,
+            updateCard: updateCard,
+          );}
+        else{
+          return MainCard(
+            card: cards[index],
+            personal: false,
+            updateCard: updateCard,
+          );}
+      }
+      else{
+        return Center(child: LoadingAnimationWidget.prograssiveDots(color: AppColors.aubRed, size: 30));
+      }
+    },
+  );
+}
--- lib/views/home_screen.dart
+++ lib/views/home_screen.dart
@@ -1,251 +1,91 @@
+import 'package:campus_plus/tabs/all_cards_tab.dart';
+import 'package:campus_plus/tabs/tagged_cards_tab.dart';
 import 'package:flutter/material.dart';
 import 'package:campus_plus/utils/app_colors.dart';
-import 'package:get/get.dart';
-import '../controller/card_controller.dart';
-import '../controller/data_controller.dart';
-import '../model/card_model.dart';
-import '../model/user_model.dart';
-import '../widgets/main_card.dart';
-import 'card_form_screen.dart';
-import 'package:campus_plus/views/notifications.dart';
 import 'package:campus_plus/views/schedule.dart';
-import 'package:loading_animation_widget/loading_animation_widget.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'card_form_screen.dart';
 
-class HomeScreen extends StatefulWidget {
-  const HomeScreen({super.key});
-
-  @override
-  _HomeScreenState createState() => _HomeScreenState();
-}
-
-enum TagsFilter { study, food, fun, sports}
-
-class _HomeScreenState extends State<HomeScreen> {
-  Size size = WidgetsBinding.instance.window.physicalSize /
-      WidgetsBinding.instance.window.devicePixelRatio;
-  final GlobalKey<RefreshIndicatorState> _refreshIndicatorKey = GlobalKey<RefreshIndicatorState>();
-  late DataController dataController;
-  late CardController cardController;
-  late final MyUser userInfo;
-  late Future<List<MyCard>> futureCards;
-  late List<MyCard> cards;
-  final List<String> _tags = <String>[];
-  final ScrollController scrollController = ScrollController();
-  final limit = 10;
-  bool isLoadingMoreCards = false;
-
-  @override
-  void initState() {
-    super.initState();
-    dataController = Get.put(DataController());
-    cardController = Get.put(CardController());
-    userInfo = dataController.getLocalData();
-    futureCards = getInitialCards();
-    scrollController.addListener(_scrollListener);
-  }
-
-  Future<void> _scrollListener() async {
-    if(isLoadingMoreCards) return;
-    if(scrollController.position.pixels > scrollController.position.maxScrollExtent-200 && scrollController.position.pixels> 50){
-      setState(() {
-        isLoadingMoreCards = true;
-      });
-      await getNextCards(cards[cards.length-1]);
-      setState(() {
-        isLoadingMoreCards = false;
-      });
-    }
-  }
-
-  Future<List<MyCard>> getInitialCards() async {
-    cards = await cardController.getInitialCards(limit);
-    return cards;
-  }
-
-  Future<void> getNextCards(MyCard card) async {
-    final newCards = await cardController.getNextCards(card, limit);
-    cards = cards + newCards;
-    final filteredCards = cardController.filterCards(cards, _tags);
-    setState(() {
-      futureCards = filteredCards;
-    });
-  }
-
-
-  Future<void> refreshCards() async {
-    final newCards = getInitialCards();
-    final filteredCards = cardController.filterCards(await newCards, _tags);
-    setState(() {
-      futureCards = filteredCards;
-    });
-  }
-
-  //TODO implement filtering button, that filters client side
-
-  Future<void> updateCard(String cardId) async {
-      final updatedCard = await cardController.getCardById(cardId);
-      if(updatedCard == null){
-        for(int i = 0; i< cards.length; i++){
-          if(cards[i].id == cardId){
-            cards.removeAt(i);
-          }
-        }
-      }
-      else{
-        for(int i = 0; i< cards.length; i++){
-          if(cards[i].id == cardId){
-            cards[i] = updatedCard;
-          }
-        }
-      }
-      final updatedCards = Future.delayed(const Duration(microseconds: 1), () => cards);
-      setState(() {
-        futureCards = updatedCards;
-      });
-  }
-
-  Future<void> updatePage() async {
-    final newCards = getInitialCards();
-    final filteredCards = cardController.filterCards(await newCards, _tags);
-    cards = await filteredCards;
-    await Future.delayed(const Duration(milliseconds: 300));
-    setState(() {
-      futureCards = filteredCards;
-    });
-  }
-
-  Wrap buildTagFilterChip() {
-    return Wrap(
-      spacing: 5.0,
-      children: TagsFilter.values.map((TagsFilter tag){
-        return FilterChip(
-            label: Text(tag.name),
-            selected: _tags.contains(tag.name),
-            onSelected: (bool value){
-              setState(() {
-                if (value) {
-                  if (!_tags.contains(tag.name)) {
-                    _tags.add(tag.name);
-                  }
-                } else {
-                  _tags.removeWhere((String name) {
-                    return name == tag.name;
-                  });
-                }
-              });
-            }
-            );
-      }).toList(),
-    );
-  }
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return DefaultTabController(
+      initialIndex: 0,
+      length: 5,
+      child: Scaffold(
         appBar: AppBar(
-            backgroundColor: AppColors.white,
-            automaticallyImplyLeading: false,
-            centerTitle: false,
-            title: Text(
-              "CAMPUS+",
-              style: TextStyle(
-                fontSize: 30,
-                color: AppColors.aubRed,
-              ),
+          backgroundColor: AppColors.white,
+          automaticallyImplyLeading: false,
+          centerTitle: false,
+          title: Text(
+            "CAMPUS+",
+            style: TextStyle(
+              fontSize: 30,
+              color: AppColors.aubRed,
             ),
-            elevation: 0,
-            actions: <Widget>[
-              IconButton(
-                  onPressed: () async {
-                    await Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context){
-                        return const MainCardForm();
-                      }),
-                    );
-                    refreshCards();
-                  },
-                  icon: Image.asset('assets/postIcon.png')),
-              IconButton(
-                  onPressed: () => {Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context){
-                        return const Schedule();
-                      })
-                  )},
-                  icon: Image.asset('assets/calendarIcon.png')),
-              IconButton(
-                  onPressed: () => { Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context){
-                        return Notifications();
-                      })
-                  )},
-                  icon: Image.asset('assets/notificationIcon.png')),
-            ]),
-        body: Container(
-          color: Colors.white,
-          child: FutureBuilder(
-            future: futureCards,
-            builder: (context, snapshot){
-              return RefreshIndicator(
-                  key: _refreshIndicatorKey,
-                  color: AppColors.aubRed,
-                  backgroundColor: Colors.white,
-                  strokeWidth: 2.0,
-                  displacement: 20.0,
-                  onRefresh: updatePage,
-                  child: _listView(scrollController, isLoadingMoreCards, snapshot, userInfo, updateCard, buildTagFilterChip)
-              );
-            },
-          )
-        )
+          ),
+          elevation: 0,
+          actions: <Widget>[
+            IconButton(
+                onPressed: () async {
+                  await Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context){
+                      return const MainCardForm();
+                    }),
+                  );
+                  //TODO refresh page after adding card
+                },
+                icon: Image.asset('assets/postIcon.png')),
+            IconButton(
+                onPressed: () => {Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context){
+                      return const Schedule();
+                    })
+                )},
+                icon: Image.asset('assets/calendarIcon.png')),
+          ],
+          bottom: TabBar(
+            indicator: UnderlineTabIndicator(
+              borderSide: BorderSide(width: 3.0, color: AppColors.aubRed),
+              insets: const EdgeInsets.symmetric(horizontal: 20.0),
+            ),
+            tabs: <Widget>[
+              Tab(
+                  icon: FaIcon(FontAwesomeIcons.house, color: AppColors.aubRed, size:20.0)
+              ),
+              Tab(
+                  icon: Icon(Icons.celebration ,color: AppColors.aubRed,)
+              ),
+              Tab(
+                  icon: FaIcon(FontAwesomeIcons.bookOpen, color: AppColors.aubRed, size:20.0)
+              ),
+              Tab(
+                  icon: FaIcon(FontAwesomeIcons.pizzaSlice, color: AppColors.aubRed, size:20.0)
+              ),
+              Tab(
+                icon: FaIcon(FontAwesomeIcons.futbol, color: AppColors.aubRed, size:20.0)
+              ),
+            ],
+          ),
+        ),
+        body: const TabBarView(
+          children: <Widget>[
+            AllCardsTab(),
+            TaggedCardsTab(tag: 'fun'),
+            TaggedCardsTab(tag: 'study'),
+            TaggedCardsTab(tag: 'food'),
+            TaggedCardsTab(tag: 'sports'),
+          ],
+        ),
+      ),
     );
   }
 }
 
-Widget _listView(ScrollController scrollController, bool isLoadingMoreCards, AsyncSnapshot snapshot, MyUser userInfo, Function updateCard, Function buildTagFilterChip) {
-  //TODO handle snapshot cases
-  if(!snapshot.hasData){
-    return Center(child: LoadingAnimationWidget.threeArchedCircle(color: AppColors.aubRed, size: 40));
-  }
-  if(snapshot.hasError){
-    return const Text('Something went wrong');
-  }
-  final cards = snapshot.data;
-  return
-     Column(
-        children: [
-          const SizedBox(height:20),
-          buildTagFilterChip(),
-          Expanded(child:
-            ListView.builder(
-              controller: scrollController,
-              physics: const AlwaysScrollableScrollPhysics(),
-              itemCount: isLoadingMoreCards ? cards.length+1 : cards.length,
-              itemBuilder: (context, index){
-                if(index<cards.length){
-                  if(cards[index].createdBy == userInfo.userId){
-                    return MainCard(
-                      card: cards[index],
-                      personal: true,
-                      updateCard: updateCard,
-                    );}
-                  else{
-                    return MainCard(
-                      card: cards[index],
-                      personal: false,
-                      updateCard: updateCard,
-                    );}
-                }
-                else{
-                  return Center(child: LoadingAnimationWidget.prograssiveDots(color: AppColors.aubRed, size: 30));
-                }
-              },
-            )
-          ),
-        ]
-      );
-}
+
 
 
